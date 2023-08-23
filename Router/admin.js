fd479529efa44442f6a7f2f8fcc04d407e3d942d
Ankita Thakur <ankitathakurankita7007@gmail.com>
2023-08-24 00:25:16 +0530

    add checkout routes

--- Router/admin.js
+++ Router/admin.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 const { sendNotification } = require("./firebase");
 const VendorSettlement = require("../model/Settlement");
 
-const cloudinary=require("./cloudinary")
+const cloudinary = require("./cloudinary");
 const {
   Adminauth,
   AdminAithentication,
@@ -43,9 +43,8 @@ admin.post("/login/:mobile", async (req, res) => {
   }
 });
 
-admin.post("/add",loginAuth,async (req, res) => {
+admin.post("/add", loginAuth, async (req, res) => {
   try {
-    
     const {
       name,
       email,
@@ -60,7 +59,7 @@ admin.post("/add",loginAuth,async (req, res) => {
       vendorId,
     } = req.body;
 
-    console.log(req.body)
+    console.log(req.body);
 
     const isAdmin = await Admin.findOne({ phoneNumber });
     const isUser = await userModel.findOne({ phoneNumber });
@@ -70,19 +69,17 @@ admin.post("/add",loginAuth,async (req, res) => {
         .status(409)
         .json({ message: "Already present contact customer care" });
     }
-    var  adminData 
+    var adminData;
     if (id_proof && companyLogo) {
       const uploadedIDproof = await cloudinary.uploader.upload(id_proof, {
         upload_preset: "ridedost",
-
       });
 
       const uploadedLogo = await cloudinary.uploader.upload(companyLogo, {
         upload_preset: "ridedost",
         folder: "ridedost",
       });
-     
-   
+
       if (uploadedIDproof && uploadedLogo) {
         adminData = new Admin({
           name,
@@ -95,21 +92,19 @@ admin.post("/add",loginAuth,async (req, res) => {
           thresholdvalue,
           id_proof: uploadedIDproof,
           companyLogo: uploadedLogo,
-          vendorId
+          vendorId,
         });
-        
-       const  response = await adminData.save();
-       console.log("this is created admin ID", adminData._id);
+
+        const response = await adminData.save();
+        console.log("this is created admin ID", adminData._id);
       }
     }
-
- 
 
     // const { vendorId } = adminData;
 
     const isVendor = await Admin.findOne({ _id: vendorId });
 
-    console.log("vendor",isVendor);
+    console.log("vendor", isVendor);
 
     if (isVendor.role == "vendor") {
       const superAdmin = await Admin.findOne({ role: "admin" });
@@ -124,7 +119,7 @@ admin.post("/add",loginAuth,async (req, res) => {
     }
 
     res.status(201).json({ message: "succesfully created" });
-    console.log("successful created")
+    console.log("successful created");
   } catch (err) {
     console.log("error", err);
     return res.status(500).json(err);
@@ -290,25 +285,26 @@ admin.patch("/return/:_id", AdminAithentication, async (req, res) => {
   return res.status(200).json({ message: "return to vendor..." });
 });
 
-
-
-
 admin.patch("/personalInfo/update", loginAuth, async (req, res) => {
   const payload = req.body;
-  const _id= req.body.vendorId
-  const {profileImage}=req.body
+  const _id = req.body.vendorId;
+  const { profileImage } = req.body;
 
   if (profileImage) {
     const image = await cloudinary.uploader.upload(profileImage, {
       upload_preset: "ridedost",
-  });
-  req.body.profileImage=image
-  
- }
- 
-//  console.log(req.body)
-const updateData = await Admin.findByIdAndUpdate(_id, { $set: req.body}, { new: true });
-console.log(updateData)
+    });
+    req.body.profileImage = image;
+    console.log(req.body);
+  }
+
+  //  console.log(req.body)
+  const updateData = await Admin.findByIdAndUpdate(
+    _id,
+    { $set: req.body },
+    { new: true }
+  );
+  // console.log(updateData)
   if (!updateData) {
     return res.status(400).json({ message: "something went wrong" });
   }
@@ -318,29 +314,45 @@ console.log(updateData)
 });
 
 //get data of single user
-admin.get("/personalInfo",loginAuth,async(req,res)=>{
+admin.get("/personalInfo", loginAuth, async (req, res) => {
+  const _id = req.body.vendorId;
+  console.log("id", req.body.vendorId);
+  try {
+    const vendorInfo = await Admin.find({ _id: _id });
+    console.log(_id);
+    return res
+      .status(200)
+      .json({ message: "succesfully get the data", vendorInfo });
+  } catch (error) {
+    console.error("Error approving update:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
 
- const _id= req.body.vendorId
- console.log("id", req.body.vendorId)
- try {
-  const vendorInfo=await Admin.find({_id:_id})
-  console.log(_id)
-  return res.status(200).json({ message: "succesfully get the data", vendorInfo });
- } catch (error) {
-  console.error("Error approving update:", error);
-  return res.status(500).json({ message: "Server error" });
- }
-
- 
-})
-
-
-admin.post("/checkout",loginAuth,async(req,res)=>{
-  console.log(req.body)
-  const vender_id= req.body.vendorId 
-  const data = await userModel.findOne({ mobile: req.body.phoneNumber });
-  const thresholdvalue = await admin.findOne({ _id: vender_id });
-  console.log(data,thresholdvalue)
-})
-
+admin.post("/checkout", loginAuth, async (req, res) => {
+  console.log(req.body);
+  const vendor_id = req.body.vendorId;
+  const data = await userModel.find({ mobile: req.body.phoneNumber });
+  const thresholdvalue = await Admin.find({ _id: vendor_id });
+  try {
+    if (data && data.length > 0) { // Check if data is not empty
+      console.log(thresholdvalue[0].thresholdvalue);
+      if (thresholdvalue && thresholdvalue.length > 0 && thresholdvalue[0].thresholdvalue < req.body.amount) {
+        const discount = thresholdvalue[0].thresholdvalue * (thresholdvalue[0].presentageValue / 100);
+        req.body.amount = req.body.amount - discount;
+        req.body.coupon = coponCode.generate();
+        console.log("Discount applied.");
+      } else {
+        console.log("Threshold condition not met.");
+      }
+    } else {
+      return res.status(400).json({ message: "Mobile number is not registered." }); // 400 Bad Request
+    }
+    console.log(req.body);
+    return res.status(200).json(req.body); // 200 OK
+  } catch (error) {
+    console.log("Error:", error);
+    return res.status(500).json({ error: "An error occurred." }); // 500 Internal Server Error
+  }
+});
 module.exports = admin;
